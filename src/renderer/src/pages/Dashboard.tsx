ecee1f10992d841898e1ee00dfd3332bbae29f10
Rizal <zalnugroho1@gmail.com>
2026-02-10 14:24:40 +0700

    feat(phar): refactor page folder

--- src/renderer/src/pages/Dashboard.tsx
+++ src/renderer/src/pages/Dashboard.tsx
@@ -189,7 +189,7 @@ const items = [
       { label: 'Permintaan Obat (Resep)', key: '/dashboard/medicine/medication-requests', icon: <DashboardOutlined /> },
       { label: 'Penyerahan Obat ', key: '/dashboard/medicine/medication-dispenses', icon: <DashboardOutlined /> },
       { label: 'Kategori Item', key: '/dashboard/medicine/medicine-categories', icon: <DashboardOutlined /> },
-      { label: 'Obat dan Barang', key: '/dashboard/farmasi/items', icon: <DashboardOutlined /> },
+      { label: 'Obat dan Barang', key: '/dashboard/medicine/items', icon: <DashboardOutlined /> },
       { label: 'Transaksi Penjualan Barang', key: '/dashboard/medicine/item-purchase', icon: <DashboardOutlined /> }
     ]
   },
