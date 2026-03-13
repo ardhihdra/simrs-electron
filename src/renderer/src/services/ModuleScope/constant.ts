78f2f53fbacd4955228b582d39360d93a38ababa
falah isnan <moh.falah.isnan@gmail.com>
2026-03-13 10:26:38 +0700

    refactor: move module definition to shared

--- src/renderer/src/services/ModuleScope/constant.ts
+++ src/renderer/src/services/ModuleScope/constant.ts
@@ -1,3 +1,4 @@
+import { ModuleDefinition } from 'simrs-types'
 import { ModuleNodeInput } from './type'
 import { buildModuleTree } from './utils'
 
@@ -15,66 +16,6 @@ const workspaceModules: ModuleNodeInput[] = Object.values(workspaceModuleCodes).
   code
 }))
 
-const domainModules: ModuleNodeInput[] = [
-  {
-    code: 'rekam_medis',
-    children: [
-      {
-        code: 'rekam_medis'
-      },
-      {
-        code: 'registration',
-        children: [{ code: 'pendaftaran' }, { code: 'penjadwalan' }, { code: 'patient' }]
-      },
-      {
-        code: 'queue'
-      }
-    ]
-  },
-  {
-    code: 'rawat_jalan',
-    children: [
-      {
-        code: 'poli',
-        children: [
-          { code: 'anak' },
-          { code: 'jantung' },
-          { code: 'kulit_dan_kelamin' },
-          { code: 'mata' },
-          { code: 'bedah' },
-          { code: 'saraf' },
-          { code: 'ortopedi' },
-          { code: 'penyakit_dalam' },
-          { code: 'kandungan' },
-          { code: 'gigi_dan_mulut' },
-          { code: 'tht' },
-          { code: 'paru' },
-          { code: 'psykologi' },
-          { code: 'fisioterapi' },
-          { code: 'gizi' },
-          { code: 'umum' }
-        ]
-      }
-    ]
-  },
-  {
-    code: 'rawat_inap',
-    children: [
-      {
-        code: 'ranap_1',
-        children: [{ code: 'class_1' }, { code: 'class_2' }, { code: 'class_3' }, { code: 'vip' }]
-      },
-      {
-        code: 'ranap_2',
-        children: [{ code: 'class_1' }, { code: 'class_2' }, { code: 'class_3' }, { code: 'vip' }]
-      }
-    ]
-  },
-  {
-    code: 'igd'
-  }
-] as const
-
-export const modules: ModuleNodeInput[] = [...workspaceModules, ...domainModules]
+export const modules: ModuleNodeInput[] = [...workspaceModules, ...ModuleDefinition]
 
 export const moduleTree = buildModuleTree(modules)
