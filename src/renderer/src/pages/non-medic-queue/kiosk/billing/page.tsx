7cbaa08a790824387bd167ab0a35b29451b8cb47
falah isnan <moh.falah.isnan@gmail.com>
2026-03-31 15:21:33 +0700

    refactor: redesign kioska

--- src/renderer/src/pages/non-medic-queue/kiosk/billing/page.tsx
+++ src/renderer/src/pages/non-medic-queue/kiosk/billing/page.tsx
@@ -2,15 +2,15 @@ import { WalletOutlined } from '@ant-design/icons'
 import ServiceKioskPage from '..'
 
 function NonMedicQueueBillingKioskPage() {
-    return (
-        <ServiceKioskPage
-            title="KIOSK Billing"
-            description="Halaman sentuh untuk pengambilan nomor antrean billing dengan tampilan besar dan fokus pada interaksi cepat."
-            serviceTypeCode="BILLING"
-            serviceLabel="Billing"
-            icon={<WalletOutlined className="text-2xl" />}
-        />
-    )
+  return (
+    <ServiceKioskPage
+      title="KIOSK Billing"
+      description="Halaman untuk pengambilan nomor antrean billing."
+      serviceTypeCode="BILLING"
+      serviceLabel="Billing"
+      icon={<WalletOutlined className="text-2xl" />}
+    />
+  )
 }
 
 export default NonMedicQueueBillingKioskPage
--- src/renderer/src/pages/non-medic-queue/kiosk/cashier/page.tsx
+++ src/renderer/src/pages/non-medic-queue/kiosk/cashier/page.tsx
@@ -2,15 +2,15 @@ import { CreditCardOutlined } from '@ant-design/icons'
 import ServiceKioskPage from '..'
 
 function NonMedicQueueCashierKioskPage() {
-    return (
-        <ServiceKioskPage
-            title="KIOSK Kasir"
-            description="Halaman sentuh untuk pengambilan nomor antrean kasir dengan tombol besar, informasi jelas, dan fokus penggunaan di monitor touch screen."
-            serviceTypeCode="CASHIER"
-            serviceLabel="Kasir"
-            icon={<CreditCardOutlined className="text-2xl" />}
-        />
-    )
+  return (
+    <ServiceKioskPage
+      title="KIOSK Kasir"
+      description="Halaman untuk pengambilan nomor antrean kasir."
+      serviceTypeCode="CASHIER"
+      serviceLabel="Kasir"
+      icon={<CreditCardOutlined className="text-2xl" />}
+    />
+  )
 }
 
 export default NonMedicQueueCashierKioskPage
