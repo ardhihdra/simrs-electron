eaab29fdd8a7cd58e905aba3b50e4288b962c332
falah isnan <moh.falah.isnan@gmail.com>
2026-04-09 14:30:08 +0700

    fix: fix string to use modules enum

--- src/renderer/src/pages/Dashboard.tsx
+++ src/renderer/src/pages/Dashboard.tsx
@@ -65,7 +65,7 @@ type DashboardMenuChild = {
 }
 
 type DashboardMenuItem = DashboardMenuChild & {
-  module?: string
+  module?: Module
   children?: DashboardMenuChild[]
 }
 
@@ -81,7 +81,7 @@ const items: DashboardMenuItem[] = [
     label: 'Registrasi',
     key: '/dashboard/registration',
     icon: <CalendarOutlined />,
-    module: 'REGISTRASI',
+    module: Modules.REGISTRASI,
     children: [
       { label: 'Pasien', key: '/dashboard/patient', icon: <UserOutlined /> },
       {
@@ -126,7 +126,7 @@ const items: DashboardMenuItem[] = [
     label: 'Rawat Jalan',
     key: '/dashboard/poli',
     icon: <CalendarOutlined />,
-    module: 'RAWAT_JALAN',
+    module: Modules.RAWAT_JALAN,
     children: [
       { label: 'Poli', key: '/dashboard/poli', icon: <CalendarOutlined /> },
       {
@@ -150,7 +150,7 @@ const items: DashboardMenuItem[] = [
     label: 'Rawat Inap',
     key: '/dashboard/rawat-inap',
     icon: <CalendarOutlined />,
-    module: 'RAWAT_INAP',
+    module: Modules.RAWAT_INAP,
     children: [
       {
         label: 'Rawat Inap 1',
@@ -168,7 +168,7 @@ const items: DashboardMenuItem[] = [
     label: 'Kamar Operasi (OK)',
     key: '/dashboard/ok',
     icon: <FileTextOutlined />,
-    module: 'OK',
+    module: Modules.OK,
     children: [
       {
         label: 'Pengajuan OK',
@@ -186,7 +186,7 @@ const items: DashboardMenuItem[] = [
     label: 'Farmasi',
     key: '/dashboard/medicine',
     icon: <WalletOutlined />,
-    module: 'FARMASI',
+    module: Modules.FARMASI,
     children: [
       { label: 'Dashboard Obat', key: '/dashboard/medicine', icon: <MedicineBoxOutlined /> },
       {
@@ -219,7 +219,7 @@ const items: DashboardMenuItem[] = [
     label: 'Laboratorium',
     key: '/dashboard/laboratory-management',
     icon: <ExperimentOutlined />,
-    module: 'LAB',
+    module: Modules.LAB,
     children: [
       {
         label: 'Antrian',
@@ -247,7 +247,7 @@ const items: DashboardMenuItem[] = [
     label: 'Kasir & Billing',
     key: '/dashboard/kasir',
     icon: <WalletOutlined />,
-    module: 'BILLING_KASIR',
+    module: Modules.BILLING_KASIR,
     children: [
       {
         label: 'Tagihan Pasien',
@@ -260,7 +260,7 @@ const items: DashboardMenuItem[] = [
     label: 'Antrian Non-Medis',
     key: '/dashboard/non-medic-queue',
     icon: <UnorderedListOutlined />,
-    module: 'BILLING_KASIR',
+    module: Modules.BILLING_KASIR,
     children: [
       {
         label: 'KIOSK Billing',
@@ -308,7 +308,7 @@ const items: DashboardMenuItem[] = [
     label: 'Sistem',
     key: '/dashboard/pegawai',
     icon: <DashboardOutlined />,
-    module: 'SYSTEM_ADMIN',
+    module: Modules.SYSTEM_ADMIN,
     children: [
       {
         label: 'Data Petugas Medis',
