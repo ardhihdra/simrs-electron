07624d31494d91ea3f6fe46e4a32b89dd9c4f416
Ardhi <ardhi.rofi@gmail.com>
2026-04-15 20:37:38 +0700

    feat(outpatient-reporting): add frontend RPC procedure

---
+++ src/main/rpc/procedure/outpatient-reporting.ts
@@ -0,0 +1,40 @@
+import z from 'zod'
+import { t } from '../'
+
+export const outpatientReportingRpc = {
+    getDashboardMetrics: t
+        .input(z.object({ date: z.string().optional() }))
+        .output(z.any())
+        .query(async ({ client }, input) => {
+            const params = new URLSearchParams()
+            if (input.date) params.append('date', input.date)
+            const data = await client.get(
+                `/api/module/outpatient-reporting/dashboard-metrics?${params.toString()}`
+            )
+            return await data.json()
+        }),
+
+    getVisitReport: t
+        .input(
+            z.object({
+                fromDate: z.string(),
+                toDate: z.string(),
+                poliCodeId: z.coerce.number().optional(),
+                practitionerId: z.coerce.number().optional(),
+                paymentMethod: z.enum(['cash', 'bpjs', 'asuransi', 'company']).optional(),
+            })
+        )
+        .output(z.any())
+        .query(async ({ client }, input) => {
+            const params = new URLSearchParams()
+            params.append('fromDate', input.fromDate)
+            params.append('toDate', input.toDate)
+            if (input.poliCodeId) params.append('poliCodeId', String(input.poliCodeId))
+            if (input.practitionerId) params.append('practitionerId', String(input.practitionerId))
+            if (input.paymentMethod) params.append('paymentMethod', input.paymentMethod)
+            const data = await client.get(
+                `/api/module/outpatient-reporting/visit-report?${params.toString()}`
+            )
+            return await data.json()
+        }),
+}
